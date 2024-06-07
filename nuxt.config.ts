c2c32538f05d162f0fbd7fd7fc64ee622909b76e
probablyjassin <aouanijassin@gmail.com>
2024-06-07 11:47:58 +0200

    tailwindcss init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
