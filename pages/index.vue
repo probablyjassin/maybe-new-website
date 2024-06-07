2d4ad8b0d52d2ae60058fe4ed1b005641681bae4
probablyjassin <aouanijassin@gmail.com>
2024-06-07 12:20:09 +0200

    add text

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,13 @@
           </template>
           <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
         </UCard>
+        <UCard class="mt-10">
+            <h1>Including:</h1>
+            <p>Nuxt Pages set up</p>
+            <p>Nuxt Ui</p>
+            <p> &lt;p class="text-red-600"&gt;<span class="text-red-600">Tailwindcss</span>&lt;/p&gt; </p>
+            <p>full color scheme</p>
+        </UCard>
       </UContainer>
     </div>
 </template>
