f6e4472388d38f23ec0d8e2f92fb1c8d18f6e21e
probablyjassin <aouanijassin@gmail.com>
2024-06-10 14:00:06 +0200

    show project info on page

--- pages/projects/[project].vue
+++ pages/projects/[project].vue
@@ -1,11 +1,19 @@
 <template>
     <div class="flex justify-center">
-        <img :src="`/images/${project}.webp`" alt="project representation" class="max-w-[70%] self-center" :style="{ 'view-transition-name': `${project}`}">
-        </div>
-    <NuxtLink to="/">zurück :3</NuxtLink>
+        <img :src="`/images/${projectName}.webp`" alt="project representation" class="max-w-[70%] self-center" :style="{ 'view-transition-name': `${projectName}`}">
+    </div>
+    <div class="flex flex-col items-center">
+        <h1 class="text-4xl">{{ project.title }}</h1>
+        <p>{{ project.description }}</p>
+        <NuxtLink to="/" class="bg-fuchsia-primary-950 hover:bg-fuchsia-primary-900 p-3 rounded-lg">zurück :3</NuxtLink>
+    </div>
 </template>
 
 <script setup>
     const route = useRoute()
-    const project = ref(route.path.split('/')[2])
+    const projects = useState('projects')
+
+    const projectName = ref(route.path.split('/')[2])
+    const project = ref(computed(() => projects.value.filter((item) => item.id == projectName.value)[0]))
 </script>
+
