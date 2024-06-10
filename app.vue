3160cc92b7232278093091fefb1b4a9f24574e40
probablyjassin <aouanijassin@gmail.com>
2024-06-10 10:27:30 +0200

    image carousel

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtPage/>
-  </div>
+	<div>
+		<NuxtPage />
+	</div>
 </template>
+
+<script setup lang="js">
+	const projects = useState('projects', (() => []));
+	const items = useState('items', (() => []));
+
+	onMounted(async () => {
+		await $fetch("/projects.json").then((response) => {
+			projects.value = response
+			projects.value.forEach(project => {
+				items.value.push({'img': `/images/${project.id}.webp`, 'to': `/projects/${project.id}`})
+			});
+		})
+	})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,100 @@
 <template>
-	<div class="h-screen p-60" style="background-image: url('/images/mos-design-V9WkSZx4TLc-unsplash.webp')">
-	<UCard class="mx-auto w-[60%] text-4xl p-5">
-		<p>Hi there, I enjoy...</p>
-		<p @mouseover="newWord(word)" class="w-full text-text-white-500">{{ word }}</p>
-		<UCarousel v-slot="{ item }" :items="items" class="carousel">
-			<img :src="item.img" width="300" height="400" draggable="false" @click="router.push(item.to)" :style="{ 'view-transition-name': `${item.to.split('/')[2]}`}" />
-		</UCarousel>
-	</UCard>
+	<div class="h-[60vh] px-[5%] py-[5vh]" style="background-image: url('/images/mos-design-V9WkSZx4TLc-unsplash.webp')">
+		<UCard class="mx-auto text-4xl p-5 flex justify-center">
+			<div class="flex align-middle snap-center text-center">
+				<img src="/images/pfp.webp" alt="avatar" class="w-20">
+				<p class="self-center text-center">Jässin</p>
+			</div>
+			<div>
+				<p>Hi there, I enjoy...</p>
+				<p @mouseover="newWord(word)" class=" text-text-white-500">{{ word }}</p>
+			</div>
+		</UCard>
 	</div>
+	<UCarousel v-slot="{ item }" :items="items" class="h-[20vh] carousel snap-none" ref="carouselRef">
+		<img
+			:src="item.img"
+			class="h-[40vh] snap-none cursor-pointer"
+			draggable="false"
+			:style="{ 'view-transition-name': `${item.to.split('/')[2]}` }"
+			@mousedown="mDown($event, item.to)"
+			@mouseup="mUp($event)"
+			indicators />
+	</UCarousel>
 </template>
 
 <script setup lang="js">
-	const router = useRouter()
+		const router = useRouter()
 
-	const word = ref("working with tech")
-	const words = ['building websites', 'working with databases', 'working with my Raspberrypi']
+		const items = useState('items')
+		const projects = useState('projects')
 
-	const randomWord = (inword) => {
-		let canidate = words[Math.floor(Math.random() * words.length)]
-		if (canidate == inword) return randomWord(inword)
-		return canidate
-	}
+		const word = ref("working with tech")
+		const words = ['building websites', 'working with databases', 'working with my Raspberrypi']
 
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-	const randomChar = () => {
-		return characters[Math.floor(Math.random() * characters.length)]
-	}
+		const randomWord = (inword) => {
+			let canidate = words[Math.floor(Math.random() * words.length)]
+			if (canidate == inword) return randomWord(inword)
+			return canidate
+		}
 
-	function newWord(inword) {
-		const newRandomWord = randomWord(inword)
-		let iterations = 0
-		const interval = setInterval(() => {
-			if (iterations >= newRandomWord.length+1) return
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+		const randomChar = () => {
+			return characters[Math.floor(Math.random() * characters.length)]
+		}
 
-			let newWord = "";
-			for (let i in newRandomWord) {
-				if (i < iterations) {
-					newWord += newRandomWord[i]
-					continue
+		function newWord(inword) {
+			const newRandomWord = randomWord(inword)
+			let iterations = 0
+			const interval = setInterval(() => {
+				if (iterations >= newRandomWord.length+1) return
+
+				let newWord = "";
+				for (let i in newRandomWord) {
+					if (i < iterations) {
+						newWord += newRandomWord[i]
+						continue
+					}
+					newWord += randomChar()
 				}
-				newWord += randomChar()
-			}
-			word.value = newWord
-			iterations += 1
-		}, 30);
-	}
+				word.value = newWord
+				iterations += 1
+			}, 30);
+		}
 
-	const items = [
-	  {"img": '/images/chessboard.webp', "to": "/projects/chessboard"},
-	  {"img": '/images/anarchychess.webp', "to": "/projects/anarchychess"},
-	]
+		const latestClick = ref([])
+		const targetLink = ref("")
+
+		function mDown(event, to) {
+			latestClick.value.push(event.screenX)
+			latestClick.value.push(event.screenY)
+			targetLink.value = to
+		}
+
+		function mUp(event) {
+			let x = event.screenX
+			let y = event.screenY
+			console.log(`diff x: ${(x - latestClick.value[0]) / latestClick.value[0]}`)
+			console.log(`diff y: ${(y - latestClick.value[1]) / latestClick.value[1]}`)
+
+			if (x == latestClick.value[0] && y == latestClick.value[1]) {
+				router.push(targetLink.value)
+			}
+			latestClick.value = []
+			targetLink.value = ""
+		}
+
+
+	const carouselRef = ref()
+	onMounted(() => {
+	  setInterval(() => {
+	    if (!carouselRef.value) return
+
+	    if (carouselRef.value.page === carouselRef.value.pages) {
+	      return carouselRef.value.select(0)
+	    }
+
+	    carouselRef.value.next()
+	  }, 3000)
+	})
 </script>
