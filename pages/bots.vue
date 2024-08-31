bf1edb0a45513b4af9675caf8b19d74af94a8856
probablyjassin <aouanijassin@gmail.com>
2024-08-31 14:15:55 +0200

    template other pages

--- pages/bots.vue
+++ pages/bots.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        bots
+    <div class="ml-[5%] mb-[5%] space-y-8">
+        <div>
+            <p class="text-3xl">seventv.py: <a href="https://7tv.app" class="text-purple-400">7tv.app</a> API wrapper</p>
+            <div class="md:flex">
+                <img src="/images/projects/seventv.webp" alt="7tv.app api wrapper" class="md: w-[50%]">
+                <div class="w-full grid grid-cols-3">
+                    <img :src="`/images/tech/mongo.png`" alt="icon" class="w-52">
+                </div>
+            </div>
+        </div>
+        <div>
+            <p class="text-3xl">Anarchychess Website</p>
+            <div class="md:flex">
+                <img src="/images/projects/anarchychess.webp" alt="anarchychess website" class="md: w-[50%]">
+                <div class="w-full grid grid-cols-3">
+					<img :src="`/images/tech/nuxt.png`" alt="icon">
+				</div>
+            </div>
+        </div>
+        <div>
+            <p class="text-3xl">Wordle-like Quiz</p>
+            <div class="md:flex">
+                <img src="/images/projects/gaugedle.webp" alt="anarchychess website" class="md: w-[50%]">
+                <div class="w-full grid grid-cols-3">
+					<img :src="`/images/tech/tailwind.png`" alt="icon">
+				</div>
+            </div>
+        </div>
     </div>
 </template>
--- pages/other.vue
+++ pages/other.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        other
+    <div class="ml-[5%] mb-[5%] space-y-8">
+        <div>
+            <p class="text-3xl">URL-Shortner</p>
+            <div class="md:flex">
+                <img src="/images/projects/nyacare.webp" alt="anarchychess website" class="md: w-[50%]">
+                <div class="w-full grid grid-cols-3">
+                    <img :src="`/images/tech/mongo.png`" alt="icon" class="w-52">
+                </div>
+            </div>
+        </div>
+        <div>
+            <p class="text-3xl">Anarchychess Website</p>
+            <div class="md:flex">
+                <img src="/images/projects/anarchychess.webp" alt="anarchychess website" class="md: w-[50%]">
+                <div class="w-full grid grid-cols-3">
+					<img :src="`/images/tech/nuxt.png`" alt="icon">
+				</div>
+            </div>
+        </div>
+        <div>
+            <p class="text-3xl">Wordle-like Quiz</p>
+            <div class="md:flex">
+                <img src="/images/projects/gaugedle.webp" alt="anarchychess website" class="md: w-[50%]">
+                <div class="w-full grid grid-cols-3">
+					<img :src="`/images/tech/tailwind.png`" alt="icon">
+				</div>
+            </div>
+        </div>
     </div>
 </template>
