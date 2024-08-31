ff39492a17016af0fa02f4dcd3c358ad81114d45
probablyjassin <aouanijassin@gmail.com>
2024-08-31 11:08:38 +0200

    nuxt trainsition

--- app.vue
+++ app.vue
@@ -17,3 +17,16 @@
 		})
 	})
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+  transform: rotate(10deg);
+}
+</style>
